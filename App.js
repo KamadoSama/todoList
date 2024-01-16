a9588a51486ec25e0198abe688e2baeb3f71b6d6
tahoarthur <tahoarthur@gmail.com>
2024-01-16 03:06:47 +0100

    Refactor tab navigation and add new screens

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import * as React from 'react';
+import * as React from "react";
 // import { Button, View } from 'react-native';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from '@expo/vector-icons/Ionicons'
-import HomeScreen from './screens/HomeScreen';
-import NotificationScreen from './screens/NotificationScreen';
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import HomeScreen from "./screens/HomeScreen";
+import NotificationScreen from "./screens/NotificationScreen";
+import AddScreen from "./screens/AddScreen";
+import FindScreen from "./screens/FindScreen";
+import CalendarScreen from "./screens/CalendarScreen";
 
 const Tab = createBottomTabNavigator();
 
@@ -15,26 +17,90 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+          tabBarShowLabel: false,
+          // tabBarIcon: ({ focused, color, size }) => {
+          //   let iconName;
 
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'ios-home'
-                : 'ios-home';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'ios-list' : 'ios-list-outline';
-            }
+          //   if (route.name === 'Home') {
+          //     iconName = focused
+          //       ? 'ios-home'
+          //       : 'ios-home';
+          //   } else if (route.name === 'Settings') {
+          //     iconName = focused ? 'ios-list' : 'ios-list-outline';
+          //   }
 
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
+          //   // You can return any component that you like here!
+          //   return <Ionicons name={iconName} size={size} color={color} />;
+          // },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
         })}
       >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={NotificationScreen} />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="ios-home"
+                size={24}
+                color={focused ? "tomato" : "gray"}
+              />
+            ),
+          }}
+          name="Home"
+          component={HomeScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="ios-search"
+                size={24}
+                color={focused ? "tomato" : "gray"}
+              />
+            ),
+          }}
+          name="Find"
+          component={FindScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="ios-add"
+                size={24}
+                color={focused ? "tomato" : "gray"}
+              />
+            ),
+          }}
+          name="Add"
+          component={AddScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="ios-calendar"
+                size={24}
+                color={focused ? "tomato" : "gray"}
+              />
+            ),
+          }}
+          name="Calendar"
+          component={CalendarScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="ios-bar-chart"
+                size={24}
+                color={focused ? "tomato" : "gray"}
+              />
+            ),
+          }}
+          name="Settings"
+          component={NotificationScreen}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
