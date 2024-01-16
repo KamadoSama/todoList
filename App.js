44ecc394ae5e4b25be351dc6a861b025439aaf8e
tahoarthur <tahoarthur@gmail.com>
2024-01-16 03:52:15 +0100

    Add custom tab bar button and styles

--- App.js
+++ App.js
@@ -9,31 +9,36 @@ import NotificationScreen from "./screens/NotificationScreen";
 import AddScreen from "./screens/AddScreen";
 import FindScreen from "./screens/FindScreen";
 import CalendarScreen from "./screens/CalendarScreen";
+import { View,TouchableOpacity,StyleSheet } from "react-native";
 
 const Tab = createBottomTabNavigator();
-
+const CustomTabBarButton = ({children,onPress}) => (
+  <TouchableOpacity
+  style={{
+    top: -30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    ...styles.shadow
+  }}
+  onPress={onPress}
+  >
+    
+    <View style={{width:60,height:60,borderRadius:20, backgroundColor:'#277dfa' }} >
+      {children}
+    </View>
+  </TouchableOpacity>
+  
+)
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={({ route }) => ({
           tabBarShowLabel: false,
-          // tabBarIcon: ({ focused, color, size }) => {
-          //   let iconName;
-
-          //   if (route.name === 'Home') {
-          //     iconName = focused
-          //       ? 'ios-home'
-          //       : 'ios-home';
-          //   } else if (route.name === 'Settings') {
-          //     iconName = focused ? 'ios-list' : 'ios-list-outline';
-          //   }
-
-          //   // You can return any component that you like here!
-          //   return <Ionicons name={iconName} size={size} color={color} />;
-          // },
-          tabBarActiveTintColor: "tomato",
-          tabBarInactiveTintColor: "gray",
+          tabBarStyle: {
+            backgroundColor: "#ffffff",
+            height: 70,
+          },
         })}
       >
         <Tab.Screen
@@ -42,7 +47,7 @@ export default function App() {
               <Ionicons
                 name="ios-home"
                 size={24}
-                color={focused ? "tomato" : "gray"}
+                color={focused ? "#277dfa" : "#b2bccd"}
               />
             ),
           }}
@@ -55,7 +60,7 @@ export default function App() {
               <Ionicons
                 name="ios-search"
                 size={24}
-                color={focused ? "tomato" : "gray"}
+                color={focused ? "#277dfa" : "#b2bccd"}
               />
             ),
           }}
@@ -63,17 +68,21 @@ export default function App() {
           component={FindScreen}
         />
         <Tab.Screen
+         name="Add"
+         component={AddScreen}
           options={{
             tabBarIcon: ({ focused }) => (
               <Ionicons
                 name="ios-add"
-                size={24}
-                color={focused ? "tomato" : "gray"}
+                size={34}
+                color={focused ? "#f3f6fd" : "#f3f6fd"}
               />
             ),
+            tabBarButton: (props) => (
+              <CustomTabBarButton {...props} />
+            )
           }}
-          name="Add"
-          component={AddScreen}
+         
         />
         <Tab.Screen
           options={{
@@ -81,7 +90,7 @@ export default function App() {
               <Ionicons
                 name="ios-calendar"
                 size={24}
-                color={focused ? "tomato" : "gray"}
+                color={focused ? "#277dfa" : "#b2bccd"}
               />
             ),
           }}
@@ -94,7 +103,7 @@ export default function App() {
               <Ionicons
                 name="ios-bar-chart"
                 size={24}
-                color={focused ? "tomato" : "gray"}
+                color={focused ? "#277dfa" : "#b2bccd"}
               />
             ),
           }}
@@ -105,3 +114,17 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: "#277dfa" ,
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 55,
+  },
+});
