679f7b69f7cb7548a21250c3c31561ce514166c8
tahoarthur <tahoarthur@gmail.com>
2024-01-28 23:16:23 +0100

    Add expo-image package and update NotificationScreen.js

--- screens/NotificationScreen.js
+++ screens/NotificationScreen.js
@@ -1,10 +1,48 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
-
+import { Button, View , StyleSheet } from 'react-native';
+import { Image } from 'expo-image';
 export default function NotificationScreen({ navigation }) {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button onPress={() => navigation.goBack()} title="Go back home" />
+      <View style={styles.container}>
+        <View style={styles.viewTop}>
+        <Image
+        style={styles.image}
+        source={require('../assets/idea.svg')}
+   
+        contentFit="cover"
+    
+      />
+        </View>
+        <View style={styles.viewBottom}>
+
+        </View>
       </View>
     );
   }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#145ac7",
+  },
+  viewTop:{
+    backgroundColor: "transparent",
+    height: "40%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  viewBottom:{
+    backgroundColor: "#fff",
+    height: "60%",
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#0553',
+  },
+  
+})
