ed2d2b3826915b1e58b53fb662494c332c309108
tahoarthur <tahoarthur@gmail.com>
2024-02-08 10:50:38 +0100

    Add French localization and implement calendar selection

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,17 +1,69 @@
-import React, { useEffect } from 'react'
-import { Button, View } from 'react-native';
-import { retrieveTasks } from "../db/crudTodo";
-import { db } from "../db/db";
-const CalendarScreen = () => {
-    useEffect(() => {
-      retrieveTasks(db);},
-       []);
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Button onPress={() => navigation.goBack()} title="Go back home" />
-        </View>
-      );
-  
-}
+import React, { useState } from "react";
+import { Button, View } from "react-native";
+import { Calendar, LocaleConfig } from "react-native-calendars";
 
-export default CalendarScreen
+LocaleConfig.locales["fr"] = {
+  monthNames: [
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre",
+  ],
+  monthNamesShort: [
+    "Janv.",
+    "Févr.",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juil.",
+    "Août",
+    "Sept.",
+    "Oct.",
+    "Nov.",
+    "Déc.",
+  ],
+  dayNames: [
+    "Dimanche",
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+  ],
+  dayNamesShort: ["Dim.", "Lun.", "Mar.", "Mer.", "Jeu.", "Ven.", "Sam."],
+  today: "Aujourd'hui",
+};
+
+LocaleConfig.defaultLocale = "fr";
+
+const CalendarScreen = () => {
+  const [selected, setSelected] = useState("");
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Calendar
+        onDayPress={(day) => {
+          setSelected(day.dateString);
+        }}
+        markedDates={{
+          [selected]: {
+            selected: true,
+            disableTouchEvent: true,
+            selectedDotColor: "orange",
+          },
+        }}
+      />
+    </View>
+  );
+};
+
+export default CalendarScreen;
